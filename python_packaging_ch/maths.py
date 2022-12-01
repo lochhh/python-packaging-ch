893e25df4ff9d023106644f07bccf1ea830d856e
lochhh <changhuan.lo@ucl.ac.uk>
2022-12-01 16:15:10 +0000

    Include fire

--- python_packaging_ch/maths.py
+++ python_packaging_ch/maths.py
@@ -1,3 +1,5 @@
+import fire
+
 from typing import Union
 
 
@@ -13,3 +15,32 @@ def subtract_numbers(
 ) -> Union[int, float]:
 
     return a - b
+
+
+def multiply_numbers(
+    a: Union[int, float], b: Union[int, float]
+) -> Union[int, float]:
+
+    return a * b
+
+
+def divide_numbers(
+    a: Union[int, float], b: Union[int, float]
+) -> Union[int, float]:
+
+    return a / b
+
+
+def power_numbers(
+    a: Union[int, float], b: Union[int, float]
+) -> Union[int, float]:
+
+    return a**b
+
+
+def main():
+    fire.Fire(add_numbers)
+
+
+if __name__ == "__main__":
+    main()
