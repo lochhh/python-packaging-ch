08df3b8754508ed0dfc1ee6fdc9f21631e2a445b
lochhh <changhuan.lo@ucl.ac.uk>
2022-12-01 14:51:15 +0000

    Add precommit

--- python_packaging_ch/maths.py
+++ python_packaging_ch/maths.py
@@ -1,8 +1,8 @@
-def add_numbers(a, b):
+def add_numbers(a: int | float, b: int | float) -> int | float:
 
     return a + b
 
 
-def subtract_numbers(a, b):
+def subtract_numbers(a: int | float, b: int | float) -> int | float:
 
     return a - b
